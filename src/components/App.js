49a6590d904fd979508cf8a5e2eca2d072e7282f
ashish <ashishmasih92g@gmail.com>
2025-05-17 21:00:16 +0530

    assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import img1 from "../assets/lambo.jpg"
+import image2 from "../assets/img2.jpg"
+import image3 from "../assets/img3.jpg"
 
+let arr = [img1,image2,image3]
 const App = () => {
+let [index,setIndex] = useState(0)
+
+
+useEffect(()=>{
+
+  let x = setInterval(() => {
+    setIndex(prev=> (prev+1 )%arr.length)
+  }, 5000);
+
+  return ()=>{
+    clearInterval(x)
+  }
+
+},[arr.length])
+ 
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
+{<img src={arr[index]} height={600} width={400} />
+    
+  }</div>
   )
 }
 
